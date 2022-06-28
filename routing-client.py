a3cc153470c6c2d058af4038a18c4b4a21cca66f
Mark Armendariz <MarkArmendariz@fico.com>
2022-06-28 02:25:01 -0700

    Updates.

--- routing-client.py
+++ routing-client.py
@@ -56,12 +56,12 @@ for i in range(len(street_list)):
         print(f'Street: {street_list[i]} and Driver: {driver_list[j]} has a SS of: {data["score"]}')
         total_score = total_score + data["score"]
     
-    print(f'Total SS: {total_score}')
+    print(f'Total SS    : {total_score}')
 
 '''
 TODO:
 Now that there is a suitability score for each street per driver the next step is to determine the combination of suitability scores
-when added together gives highest total score so as to maximize the total score over the set of drivers adhering to the rule that each
+when added together that gives highest total score so as to maximize the total score over the set of drivers adhering to the rule that each
 driver can have only one shipment and each shipment can can only be assiged to one driver. Unfortunately I ran out of time to implement
 this part.
 '''
