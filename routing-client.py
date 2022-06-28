816661e24adcc13133443eee804aa722e3a69425
Mark Armendariz <MarkArmendariz@fico.com>
2022-06-28 02:04:06 -0700

    Updates.

--- routing-client.py
+++ routing-client.py
@@ -41,17 +41,9 @@ with open(sys.argv[2], mode='r') as drivers_file:
 
 print(driver_list)
 
-# Determine if we have more streets to deliver to than drivers
-street_length = len(street_list)
-driver_length = len(driver_list)
-if street_length > driver_length:
-    list_length = driver_length
-else:
-    list_length = street_length
-
 # Determine the suitability score for each street per driver
-for i in range(list_length):
-    for j in range(list_length):
+for i in range(len(street_list)):
+    for j in range(len(driver_list)):
         PARAMS = {
             'street' :street_list[i],
             'driver': driver_list[j]
@@ -64,7 +56,7 @@ for i in range(list_length):
 
 '''
 TODO:
-Now that there is a suitability score for each street per driver the next step is to determine the combitionation of suitability scores
+Now that there is a suitability score for each street per driver the next step is to determine the combination of suitability scores
 when added together gives highest total score so as to maximize the total score over the set of drivers adhering to the rule that each
 driver can have only one shipment and each shipment can can only be assiged to one driver. Unfortunately I ran out of time to implement
 this part.
