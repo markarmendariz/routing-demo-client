d0785df7a8d367dddb794ec2a8e26e5300b0b9ed
Mark Armendariz <MarkArmendariz@fico.com>
2022-07-02 14:09:56 -0700

    Updates.

--- routing-client.py
+++ routing-client.py
@@ -2,69 +2,77 @@ import requests
 import csv
 import sys
 
-# URL to routing-demo service
-URL = "http://localhost:8080/calculateSS"
-
-if len(sys.argv) < 3:
-    print("usage: python3 routing-client.py address.txt drivers.txt")
-    exit(1)
-
-# Load streets for destinations
-street_list = []
-with open(sys.argv[1], mode='r') as address_file:
-    address_reader = csv.DictReader(address_file)
-    line_count = 0
-    for row in address_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+# Load destinations
+def load_destinations(street_filename):
+    street_list = []
+    with open(street_filename, mode='r') as street_file:
+        street_reader = csv.DictReader(street_file)
+        line_count = 0
+        for row in street_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            print(row["street name"])
+            street_list.append(row["street name"])
             line_count += 1
-        print(row["street name"])
-        street_list.append(row["street name"])
-        line_count += 1
-    print(f'Processed {line_count} street records.')
+        print(f'Processed {line_count} street records.')
 
-print(street_list)
+    return street_list    
 
 # Load drivers
-driver_list = []
-with open(sys.argv[2], mode='r') as drivers_file:
-    drivers_reader = csv.DictReader(drivers_file)
-    line_count = 0
-    for row in drivers_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+def load_drivers(drivers_filename):
+    driver_list = []
+    with open(drivers_filename, mode='r') as drivers_file:
+        drivers_reader = csv.DictReader(drivers_file)
+        line_count = 0
+        for row in drivers_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            print(row["driver"])
+            driver_list.append(row["driver"])
             line_count += 1
-        print(row["driver"])
-        driver_list.append(row["driver"])
-        line_count += 1
-    print(f'Processed {line_count} driver records.')
+        print(f'Processed {line_count} driver records.')
 
-print(driver_list)
+    return driver_list
 
-# Determine the suitability score for each street per driver
-for i in range(len(street_list)):
-    total_score = 0
-    for j in range(len(driver_list)):
-        PARAMS = {
-            'street' :street_list[i],
-            'driver': driver_list[j]
-        }
+# Main
+def main(destination_filename, driver_filename):
+    # URL to routing-demo service
+    URL = "http://localhost:8080/calculateSS"
 
-        resp = requests.get(url = URL, params = PARAMS)
-        data = resp.json()
+    # Load destinations
+    street_list = load_destinations(sys.argv[1])
+    print(street_list)
 
-        print(f'Street: {street_list[i]} and Driver: {driver_list[j]} has a SS of: {data["score"]}')
-        total_score = total_score + data["score"]
-    
-    print(f'Total SS    : {total_score}')
+    # Load drivers
+    driver_list = load_drivers(sys.argv[2])
 
-'''
-TODO:
-Now that there is a suitability score for each street per driver the next step is to determine the combination of suitability scores
-when added together that gives highest total score so as to maximize the total score over the set of drivers adhering to the rule that each
-driver can have only one shipment and each shipment can can only be assiged to one driver. Unfortunately I ran out of time to implement
-this part.
-'''
+    print(driver_list)
+
+    # Determine the suitability score for each street per driver
+    for i in range(len(street_list)):
+        total_score = 0
+        for j in range(len(driver_list)):
+            PARAMS = {
+                'street' :street_list[i],
+                'driver': driver_list[j]
+            }
+
+            resp = requests.get(url = URL, params = PARAMS)
+            data = resp.json()
+
+            print(f'Street: {street_list[i]} and Driver: {driver_list[j]} has a SS of: {data["score"]}')
+            total_score = total_score + data["score"]
+        
+        print(f'Total SS    : {total_score}')
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("usage: python3 routing-client.py destinations.txt drivers.txt")
+        exit(1)
+
+    main(sys.argv[1], sys.argv[2])
 
 
 
